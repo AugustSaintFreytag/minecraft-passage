f2677457ef9a23beed2e77aabb06978e18ffed58
August Saint Freytag <me@augustfreytag.com>
2025-12-08 19:13:46 +0100

    Load and set chunk data manager on mod init

--- src/main/java/net/saint/passage/Mod.java
+++ src/main/java/net/saint/passage/Mod.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.saint.passage.data.chunk.ChunkDataManager;
 
 public class Mod implements ModInitializer {
 
@@ -19,13 +21,23 @@ public class Mod implements ModInitializer {
 
 	public static ModConfig CONFIG;
 
+	public static ChunkDataManager CHUNK_DATA_MANAGER;
+
 	// Init
 
 	@Override
 	public void onInitialize() {
 		// Config
-
 		AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
 		CONFIG = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+
+		// Init
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+			var world = server.getOverworld();
+			var persistentStateManager = world.getPersistentStateManager();
+
+			CHUNK_DATA_MANAGER = persistentStateManager.getOrCreate(ChunkDataManager::fromNbt, () -> new ChunkDataManager(),
+					MOD_ID + "_chunk_data");
+		});
 	}
 }
