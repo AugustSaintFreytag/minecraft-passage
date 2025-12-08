59ca5d3b416f4d964ea2e387a5ff3405df05ca95
August Saint Freytag <me@augustfreytag.com>
2025-12-08 19:51:52 +0100

    Define config options for block degradation
    Add default language localization file.

--- src/main/java/net/saint/passage/ModConfig.java
+++ src/main/java/net/saint/passage/ModConfig.java
@@ -2,7 +2,6 @@ package net.saint.passage;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = Mod.MOD_ID)
@@ -11,8 +10,16 @@ public class ModConfig implements ConfigData {
 
 	// General
 
-	@ConfigEntry.Category("general")
-	@Comment("Enable path degradation for well-traded blocks.")
-	public boolean degradePaths = true;
+	@Comment("Enable block degradation for well-treaded paths in the world. (Default: true)")
+	public boolean degradeBlocks = true;
+
+	@Comment("The rough number of steps required to degrade a block into its next stage. (Default: 10)")
+	public int blockResilience = 10;
+
+	@Comment("The chance for a block that would have degraded to resist degradation instead. (Default: 0.1)")
+	public double blockResilienceJitter = 0.1;
+
+	@Comment("The factor for increased resilience of subsequent stages of degradation. (Default: 4)")
+	public double blockResilienceScalingFactor = 2.5;
 
 }
